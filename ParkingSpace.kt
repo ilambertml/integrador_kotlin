f03c1cd8c1f15a403ab4101b92782e0ee852c283
Ignacio Lambert <ilambert@uy0c02gq0auq05p.ml.com>
2022-05-24 16:33:19 -0300

    partes 3 y 4

--- ParkingSpace.kt
+++ ParkingSpace.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication.model
 
+/**
+ * parkedTime: lo agregamos de tipo Long pues es el tipo devuelto por la funcion Calendar.getInstance()
+ **/
+
 data class ParkingSpace(
     var vehicle: Vehicle,
     var parkedTime: Long
--- Vehicle.kt
+++ Vehicle.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication.model
 import com.example.myapplication.VehicleType
 /**
- * @param type: lo definimos como val porque para una placa no cambia el tipo de vehiculo en el tiempo
+ * @param
+ * type: lo definimos como val porque para una placa no cambia el tipo de vehiculo en el tiempo
+ * checkInTime: lo agregamos en Vehicle pues es una propiedad del vehiculo y su ingreso al parking
+ * discountCard: lo agregamos en Vehicle pues es una propiedad del vehiculo y su ingreso al parking; lo
+ * agregamos nullable con el ?
  */
-data class Vehicle(val plate: String,val type: VehicleType) {
+
+data class Vehicle(val plate: String,val type: VehicleType,val checkInTime: Long,val discountCard?: String) {
     override fun equals(other: Any?): Boolean {
         if (other is Vehicle) {
             return this.plate == other.plate
         }
         return super.equals(other)
     }
-
     override fun hashCode(): Int = this.plate.hashCode()
 }
