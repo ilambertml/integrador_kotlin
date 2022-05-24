afa19b4d1cec5c4113b7c154cf736f63414caa77
sabrina <sabrina.arrebillaga@mercadolibre.com>
2022-05-24 16:19:32 -0300

    add comments

--- Parking.kt
+++ Parking.kt
@@ -2,6 +2,9 @@ package com.example.myapplication.model
 
 import java.util.*
 
+/**
+ * @param vehicles: se define como un set porque es mas rapido para realizar operaciones de pertenencia, ademas de que no precisamos orden
+ */
 data class Parking(val vehicles: MutableSet<Vehicle>) {
 
 
--- Vehicle.kt
+++ Vehicle.kt
@@ -1,9 +1,9 @@
 package com.example.myapplication.model
 import com.example.myapplication.VehicleType
 /**
- * @param chekInTime
+ * @param type: lo definimos como val porque para una placa no cambia el tipo de vehiculo en el tiempo
  */
-data class Vehicle(val plate: String,var type: VehicleType) {
+data class Vehicle(val plate: String,val type: VehicleType) {
     override fun equals(other: Any?): Boolean {
         if (other is Vehicle) {
             return this.plate == other.plate
