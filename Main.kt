ecae4c8ebd25c8c5291e9d0ca9bac7f8452196e5
sabrina <sabrina.arrebillaga@mercadolibre.com>
2022-05-24 16:07:37 -0300

    add main and VehiclesType

---
+++ Main.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.model
+
+    fun main() {
+
+        val parking = Parking(mutableSetOf())
+
+    }
---
+++ VehicleType.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+enum class VehicleType(var price: Int) {
+
+    CAR(20),
+    MOTORCYCLE(15),
+    MINIBUS(25),
+    BUS(30)
+
+}
