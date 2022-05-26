474eb68be751de489fb03975c95e847643404383
sabrina <sabrina.arrebillaga@mercadolibre.com>
2022-05-26 14:25:59 -0300

    changes excercise seven

--- Main.kt
+++ Main.kt
@@ -6,27 +6,70 @@ import java.util.*
 fun main() {
 
 
-    val car = Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+   /* val car =
+        Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val motorCycle =
+        Vehicle("BBBB111", VehicleType.MOTORCYCLE, Calendar.getInstance().timeInMillis, null)
+    val miniBus = Vehicle("CCCC111", VehicleType.MINIBUS, Calendar.getInstance().timeInMillis, null)
+    val bus = Vehicle("B2B2B111", VehicleType.BUS, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD002")
+    //repeat car
+    val car2 = Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+
+
+     val parking = Parking(vehiclesSet)
+     println(parking.vehicles.contains(car))
+     println(parking.vehicles.contains(motorCycle))
+     println(parking.vehicles.contains(miniBus))
+     println(parking.vehicles.contains(bus))
+
+     val isCar2Inserted = parking.vehicles.add(car2)
+     println("Car 2 repeted is inserted: "+isCar2Inserted)
+ */
+
+
+    val car1 = Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car2 = Vehicle("AAAA112", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car3 = Vehicle("AAAA113", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car4 = Vehicle("AAAA114", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car5 = Vehicle("AAAA115", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car6 = Vehicle("AAAA116", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car7 = Vehicle("AAAA117", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car8 = Vehicle("AAAA118", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car9 = Vehicle("AAAA119", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car10 = Vehicle("AAAA120", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car11 = Vehicle("AAAA121", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car12 = Vehicle("AAAA122", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car13 = Vehicle("AAAA123", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car14 = Vehicle("AAAA124", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car15= Vehicle("AAAA125", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car16 = Vehicle("AAAA126", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    val car17 = Vehicle("AAAA127", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
     val motorCycle = Vehicle("BBBB111", VehicleType.MOTORCYCLE, Calendar.getInstance().timeInMillis, null)
     val miniBus = Vehicle("CCCC111", VehicleType.MINIBUS, Calendar.getInstance().timeInMillis, null)
     val bus = Vehicle("B2B2B111", VehicleType.BUS, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD002")
 
     //repeat car
-    val car2 = Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
-
-
-    val parking = Parking(mutableSetOf(car, motorCycle, miniBus, bus))
-
-    println(parking.vehicles.contains(car))
-    println(parking.vehicles.contains(motorCycle))
-    println(parking.vehicles.contains(miniBus))
-    println(parking.vehicles.contains(bus))
-
-    val isCar2Inserted = parking.vehicles.add(car2)
-    println("Car 2 repeted is inserted: "+isCar2Inserted)
+    //val car2 = Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
 
     //put vehicles into parking. Test fun add
-    val car3 = Vehicle("AAAA222", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
-    if (parking.addVehicle(car3)) println("Welcome to AlkeParking!") else println("Sorry, the check-in failed")
+    var arrayVehicles = arrayOf(car1,car2,car3,car4,car5,car6,car7,car8,car9,car10,car11,car12,car13,car14,car15,car16,car17,motorCycle,miniBus,bus,car17)
+    var i = 1;
+    for (vh in arrayVehicles){
+        if (Parking.addVehicle(vh)) println(""+i+"-Welcome to AlkeParking!") else println(""+i+"-Sorry, the check-in failed")
+        i++;
+    }
+
+    val onSucces: (Int)-> Unit = {
+        println("Your fee is $$it. Come back soon.")
+        }
+
+    val onError: ()-> Unit = {println("error")}
+
+    //Calendar.getInstance().timeInMillis-car.checkInTime
+    val car =Vehicle("AAAA111", VehicleType.CAR, Calendar.getInstance().timeInMillis, "DISCOUNT_CARD001")
+    ParkingSpace(car,  11268000).checkOutVehicle(car.plate, onSucces, onError) //remove car
+
+    println("parking vehicles " + Parking.vehiclesList)
 
 }
+
--- ParkingSpace.kt
+++ ParkingSpace.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.model
 
+import com.example.myapplication.VehicleType
+
 /**
  * parkedTime: lo agregamos de tipo Long pues es el tipo devuelto por la funcion Calendar.getInstance()
  **/
@@ -7,6 +9,34 @@ package com.example.myapplication.model
 data class ParkingSpace(
     var vehicle: Vehicle,
     var parkedTime: Long
-){
+) {
 
+    //fun getParkedTime(chekInTime:Long) = Calendar.getInstance().timeInMillis - chekInTime
+
+    fun checkOutVehicle(plate:String, onSucces: (Int) -> Unit, onError: () -> Unit) {
+        (Parking.searchVehicle(plate))?.let {
+            var hasDiscount = false
+            it.discountCard.let { hasDiscount=true }
+            if (Parking.removeVehicle(it)) onSucces(800) else onError()
+        } ?: onError()
+    }
+
+    /**
+     * calculate value to pay
+     * @param type: Type of Vehicle
+     * @param parkedTime: Time transcurrido
+     */
+    fun calculateFee(type: VehicleType, parkedTime: Long, hasDiscountCard: Boolean):Int{
+        var fee = 0
+        when{
+            parkedTime <= 7200000 -> fee = (type.price)
+            parkedTime > 7200000 -> {
+                val minutes = (parkedTime-7200000) / 1000 / 60
+                println("ver"+minutes)
+                fee =(type.price)+(minutes / 15).toInt() * 5
+            }
+        }
+        println("time parquimetro $parkedTime")
+        return fee
+    }
 }
